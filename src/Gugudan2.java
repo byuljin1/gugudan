b2287aaf3d4f612f0a38b82b46f322e1930d3950
byuljin1 <bjkimg@gmail.com>
2020-03-02 23:09:57 +0900

    1

---
+++ src/Gugudan2.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Gugudan2 {
+	public static void main(String[] args) {
+		int[] result = new int[9];
+		for(int i=0; i<result.length; i++) {
+			result[i] = 2 * (i + 1);
+		}
+		
+		for(int i=0; i < result.length; i++) {
+			System.out.println(result[i]);
+		}
+		
+		result = new int[9];
+		for(int i=0; i<result.length; i++) {
+			result[i] = 3 * (i + 1);
+		}
+		
+		for(int i=0; i < result.length; i++) {
+			System.out.println(result[i]);
+		}
+	}
+}
